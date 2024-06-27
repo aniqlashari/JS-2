016d8cbbfea5dadca087204319984dfd72a6a933
aniqlashari <mahaqamber3@gmail.com>
2024-06-27 20:37:25 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,37 @@
+// // alert  ("New Work of New Day")
+
+// // var num1 =+ prompt ("" , "")
+
+// // var num2 =+ prompt ("" , "")
+
+// // var num = 1
+// // alert ("first alurt" **num)
+// // alert (num)
+// // alert (num)
+// // alert (num)
+// // alert (num)
+// // alert (num)
+// // alert (num)
+// // alert (num1*num2*num3)
+// // alert (num1*num2*num3)
+// // alert (num1*num2*num3)
+// // alert (num1*num2*num3)
+
+// var num1 = prompt ("eanter your name")
+// alert ("hello" +  num1 +!)
+// var X =+ prompt ("enter your numer ")
+// if (4 != 2){
+// alert ("kia hona cahie no 2")
+// }
+var num = prompt ("enter your numer")
+
+alert (num + " X" + " 1" + " = " + num * 1)
+alert (num + " X" + " 2" + " = " +  num * 2)`
+alert (num + "  X" + " 3" + " = " +  num * 3)
+alert (num + " X" +  " 4" + " = " +  num * 4)
+alert (num + "  X" + " 5" + " = " +  num * 5)
+alert (num + " X" + " 6" + " = " +  num * 6)
+alert (num + " X" + " 7" + " = " +  num * 7)
+alert (num + " X" + " 8" + " = " +  num * 8)
+alert (num + " X" + " 9" + " = " +  num * 9)
+alert (num + " X" + " 10"+ " = " +  num * 10)`
